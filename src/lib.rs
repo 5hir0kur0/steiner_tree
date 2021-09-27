df038e40481fdf74d6318abf3adf326f44cea84f
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2021-09-27 17:29:08 +0200

    fix ambiguous import

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,8 @@ mod steiner_tree;
 mod util;
 
 pub use graph::Graph;
-pub use steiner_tree::algorithms::dreyfus_wagner;
-pub use steiner_tree::algorithms::kou_et_al_steiner_approximation;
-pub use steiner_tree::algorithms::takahashi_matsuyama_steiner_approximation;
-pub use steiner_tree::algorithms::takahashi_matsuyama_steiner_approximation_serial;
-pub use steiner_tree::tree::EdgeTree;
+pub use crate::steiner_tree::algorithms::dreyfus_wagner;
+pub use crate::steiner_tree::algorithms::kou_et_al_steiner_approximation;
+pub use crate::steiner_tree::algorithms::takahashi_matsuyama_steiner_approximation;
+pub use crate::steiner_tree::algorithms::takahashi_matsuyama_steiner_approximation_serial;
+pub use crate::steiner_tree::tree::EdgeTree;
